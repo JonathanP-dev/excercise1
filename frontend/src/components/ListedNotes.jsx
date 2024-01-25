ca6bc648779994ad1259da4403eb5815abb22146
Jonathan Pereira <106771760+JonathanP-dev@users.noreply.github.com>
2024-01-24 22:09:53 -0300

    Notes view added, basic styles added too

--- frontend/src/components/ListedNotes.jsx
+++ frontend/src/components/ListedNotes.jsx
@@ -4,17 +4,17 @@ import { Note } from './Note'
 export function ListedNotes () {
     const [notes, setNotes] = useState([])
     
-    // useEffect(()=>{
-    //   async function getNotes() {
-    //     const res = await fetch('http://localhost:5000/api/notes')
-    //     const data  = await res.json()
-    //     console.log(data)
-    //     setNotes(data.filter(note => note.archived == false))
-    //   }
-    //   getNotes()
-    // },[])
+    useEffect(()=>{
+      async function getNotes() {
+        const res = await fetch('http://localhost:5000/api/notes')
+        const data  = await res.json()
+        console.log(data)
+        setNotes(data.filter(note => note.archived == false))
+      }
+      getNotes()
+    },[])
     return(
-      <section>
+      <section className='notes-list-container'>
         {
           !notes 
             ? <span>Loading...</span>
--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -2,7 +2,20 @@
 
 export function Note ({note}) {
 
+  const handleDelete = () => {
+    
+  }
   return (
-    <article>{note.title}</article>
+    <article className='note-container'>
+      <section className='note-title-container'>
+        <h3 className='note-title'>{note.title}</h3>
+        <p className='note-content'>{note.content}</p>
+      </section>
+      <section className='note-btn-container'>
+        <button className='note-btn'>Edit</button>
+        <button className='note-btn'>Archive</button>
+        <button onClick={handleDelete} className='note-btn btn-delete'>Delete</button>
+      </section>
+    </article>
   )
 }
